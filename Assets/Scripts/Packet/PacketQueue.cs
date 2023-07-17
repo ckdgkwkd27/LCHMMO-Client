949c93664b544c6e795c30dbad29726dde310932
ckdgkwkd27 <ckdgkwkd27@com2us.com>
2023-07-17 20:16:43 +0900

    0717

--- Assets/Scripts/Packet/PacketQueue.cs
+++ Assets/Scripts/Packet/PacketQueue.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Security.Permissions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,5 +14,41 @@ public class PacketMessage
 
 public class PacketQueue
 {
-    
+    public static PacketQueue Instance { get; } = new PacketQueue();
+    Queue<PacketMessage> _packetQueue = new Queue<PacketMessage>();
+    object _lock = new object();
+
+    public void Push(ushort id, IMessage packet)
+    {
+        lock(_lock)
+        {
+            _packetQueue.Enqueue(new PacketMessage() { Id = id, Message = packet });
+        }
+    }
+
+    public PacketMessage Pop()
+    {
+        lock(_lock)
+        {
+            if (_packetQueue.Count == 0)
+                return null;
+
+            return _packetQueue.Dequeue();
+        }
+    }
+
+    public List<PacketMessage> PopAll()
+    {
+        List<PacketMessage> list = new List<PacketMessage>();
+
+        lock(_lock)
+        {
+            while(_packetQueue.Count > 0 )
+            {
+                list.Add(_packetQueue.Dequeue());
+            }
+        }
+
+        return list;
+    }
 }
--- Assets/Scripts/Packet/ServerSession.cs
+++ Assets/Scripts/Packet/ServerSession.cs
@@ -3,10 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
-namespace Assets.Scripts.Packet
-{
-    internal class ServerSession
-    {
-    }
-}
+
